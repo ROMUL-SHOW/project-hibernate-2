427eeab56e8da014c284c5e9309f70e2155e4d0a
ROMUL-SHOW <Craz9.Bunn9@yandex.ru>
2025-02-12 01:10:38 +0300

    refactor: added initComponents for all controllers in TableController

--- src/main/java/org/movie/Main.java
+++ src/main/java/org/movie/Main.java
@@ -2,9 +2,11 @@ package org.movie;
 
 import org.movie.presentation.controller.TableController;
 
+
 public class Main {
     public static void main(String[] args) {
         TableController tableController = new TableController();
+        tableController.initComponents();
         tableController.start();
     }
 }
